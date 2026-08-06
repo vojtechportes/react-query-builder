6907acc67ae84c3393004071df3eac8b158b236d
Vojtěch Václav Porteš <vojtech.v.portes@gmail.com>
2026-08-06 14:59:28 +0200

    feat: Added an option to update documentation hashes programatically

--- website/src/v2/pages/documentation-page/constants/documentation-baseline.ts
+++ website/src/v2/pages/documentation-page/constants/documentation-baseline.ts
@@ -2,145 +2,97 @@ export const documentationBaseline = [
   {
     path: '/documentation',
     title: 'Documentation Overview',
-    contentHash:
-      '7e9b2c3e8f9e8d75dbe2d388b190db3619d40e7faf1f98dd96ebc7f3fa1ee262',
   },
   {
     path: '/documentation/installation',
     title: 'Installation',
-    contentHash:
-      '26f755387de8956b275296a1e6023355e4187b4ae5b8f516910e1c7d8b773438',
   },
   {
     path: '/documentation/migration-to-2-0',
     title: 'Migration to 2.0',
-    contentHash:
-      '381e81a33917a8b9e6ef175b660f232088fc98a3763bec5edec3f109b501c277',
   },
   {
     path: '/documentation/usage',
     title: 'Usage',
-    contentHash:
-      '9f42cdb8f36b3552a82257f755e2b7411c46bfa06362a4ce8119a612fb621dce',
   },
   {
     path: '/documentation/builder-behavior',
     title: 'Builder Behavior',
-    contentHash:
-      '333e0280674d9cb807fe7f9ec570ab9b67ced34e27e41d087972507da12fa8cb',
   },
   {
     path: '/documentation/builder-ref',
     title: 'Builder Ref',
-    contentHash:
-      '362e1499b9c686152b0c8d64789793ec26b6d61839909c4886b6810681e2b3bd',
   },
   {
     path: '/documentation/field-comparisons',
     title: 'Field Comparisons',
-    contentHash:
-      'aff5a2c22a402364df01f35ee1c069467c7177123a2dbf050158c776d193653d',
   },
   {
     path: '/documentation/dynamic-field-options',
     title: 'Dynamic Field Options',
-    contentHash:
-      '12b2e254cb3f8a1680c2d98268b240a79b87d6f5be03fc3eeef0ea8aae3181e0',
   },
   {
     path: '/documentation/validation',
     title: 'Validation',
-    contentHash:
-      '1e86ba1dfbd7ad79c8e958ce165e3df001916dd381bbfb3019e27384f0b40b49',
   },
   {
     path: '/documentation/history',
     title: 'Undo and Redo',
-    contentHash:
-      '917d165e545f00e95c19bcd5d16667adaf917a5284b472b96836a7126e91bee3',
   },
   {
     path: '/documentation/locking-and-read-only',
     title: 'Locking and Read-only',
-    contentHash:
-      '2c72c33f2d903d9e7ef94b7f9204333972d4d9b5cff0950d9b13756a5871a73b',
   },
   {
     path: '/documentation/parsing-and-formatting',
     title: 'Overview',
-    contentHash:
-      '0b57e0266e2066de98cedb214072ff27b6ee157a35e03a889a9797717b5f4783',
   },
   {
     path: '/documentation/parsing-and-formatting/supported-formats',
     title: 'Supported Formats',
-    contentHash:
-      '8bfa7855bc503b2eee4b48533b9ce5cb1ce1a669198a57dbfb6c1c18b01689b1',
   },
   {
     path: '/documentation/text-mode',
     title: 'Text Mode',
-    contentHash:
-      'ce08d806ac003bf74042534050340918305b81f5fe4168315ec697d9e32d8087',
   },
   {
     path: '/documentation/components',
     title: 'Components',
-    contentHash:
-      '2d158056043f93efdfb76ec15294934dd4d99ab4f642de4358bc74b46242ac55',
   },
   {
     path: '/documentation/adapters',
     title: 'Adapters',
-    contentHash:
-      '9babf32fce1bd29b0beb124636d98260b9e8bff717ad72443580d210dbcb4f70',
   },
   {
     path: '/documentation/adapters/mui',
     title: 'MUI',
-    contentHash:
-      '4be6cf45b2ebb9f8c671401c63b1ea2340ce6292705933984216b1ac26f32c13',
   },
   {
     path: '/documentation/adapters/antd',
     title: 'ANTD',
-    contentHash:
-      'daab9c9a0c910410ad7ee63ca93c1df1d42f06803f18e36d3f7e60b63322cf66',
   },
   {
     path: '/documentation/adapters/fluentui',
     title: 'Fluent UI',
-    contentHash:
-      '2ccbb0c4441b08ac03a65d8cebed7c49e815063f0f72b650b5c096ec31eed8c4',
   },
   {
     path: '/documentation/adapters/mantine',
     title: 'Mantine',
-    contentHash:
-      '4afc2211677a2d15cfce65a257bdf5852ce8eadfd47d7a8c0e41537335168f70',
   },
   {
     path: '/documentation/adapters/bootstrap',
     title: 'Bootstrap',
-    contentHash:
-      'ec14430149ab3bf70d7c282457a0fd0d994c3f0d1dcceff75224a2ae5e93db46',
   },
   {
     path: '/documentation/adapters/radix',
     title: 'Radix',
-    contentHash:
-      'a07fa7486890a753cfa272c1255dc279017191ad4f6a74f180822c9ce76e18af',
   },
   {
     path: '/documentation/theming',
     title: 'Theming',
-    contentHash:
-      'c9f5e9b82445e3ddfe7a04506a0c84c571021de240b7cfa4995c0446ebeb211b',
   },
   {
     path: '/documentation/localization',
     title: 'Localization',
-    contentHash:
-      'c3301e706ae607ca391dabe73f52f42f0dbf009d6f05fd310c9fe08e62454c33',
   },
 ] as const;
